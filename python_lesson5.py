f6a01515d31c9cbc235122a67f5ef15ad19ae4d5
Assics99 <razvozzaevivan@gmail.com>
2025-03-31 20:17:32 +0900

    Add files via upload

---
+++ python_lesson5.py
@@ -0,0 +1,70 @@
+import random
+from faker import Faker
+import file_operations
+
+skills = ["Стремительный прыжок",
+"Электрический выстрел",
+"Ледяной удар",
+"Стремительный удар",
+"Кислотный взгляд",
+"Тайный побег",
+"Ледяной выстрел",
+"Огненный заряд"]
+
+runic_dictionary = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+runic_skills = []
+
+for skill in skills:
+    text = ''
+    for letter in skill:
+        text += runic_dictionary.get(letter)
+    runic_skills.append(text)
+
+skills_random = random.sample(runic_skills,3)
+fake = Faker("ru_RU")
+
+def main(): 
+    for number in range(1,11):
+        context = {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": skills_random[0],
+        "skill_2": skills_random[1],
+        "skill_3": skills_random[2]
+        }
+        file_operations.render_template("charsheet.svg", "results/result-{}.svg".format(number), context)
+
+if __name__ == '__main__':
+    main()
